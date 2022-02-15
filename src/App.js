cb112e7dc054f5d011de3cfb491309d350d8d2d2
thanhdan0811 <thanhdannguyen@deevotech.com>
2022-02-15 14:28:14 +0700

    update menu and navigate use withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import React from "react";
 import Homepage from "./pages/homepage/homepage.compoent";
+import { Route } from "react-router-dom";
 
-function App() {
+function App(props) {
+  const HatsPage = () => {
+    return (
+      <div>
+        <h1>Hats Page</h1>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={HatsPage} />
+
+      {/* <Homepage /> */}
+      {/* 
+          component là component mà ta muốn render
+          path là đường dẫn cần xác định để render component.
+          exact là true hoặc false, path phải chính xác với chuỗi string.
+        */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+// React router dom
+import { BrowserRouter } from "react-router-dom";
+// BrowserRouter : bao quanh App component.
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
